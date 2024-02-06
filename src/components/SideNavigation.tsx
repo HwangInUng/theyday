d014f6981bf26ef6c0c2870f4fd617ec0f1fff1e
HwangInUng <ung6860@naver.com>
2024-02-07 07:39:05 +0900

    first commit

---
+++ src/components/SideNavigation.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import MainLogo from "../assets/main-logo.png";
+import { BsCartFill } from "react-icons/bs";
+
+const SideNavigation = () => {
+  const menu = ["물품관리", "매출관리", "예약관리", "재료관리"];
+  return (
+    <div className="flex flex-col w-[250px] h-screen bg-white border-r rounded-r-lg overflow-hidden border-orange-100 flex-shrink-0 shadow-lg shadow-orange-300">
+      <div className="h-[150px] flex justify-center items-center">
+        <Image
+          src={MainLogo}
+          alt="main logo"
+          className="w-[100px] h-[100px] rounded-[50%] border border-orange-200"
+        />
+      </div>
+      <div className="flex-grow">
+        <ul className="h-full flex flex-col gap-y-2 text-lg py-2 pr-2">
+          {menu.map((m, index) => (
+            <li
+              key={index}
+              className="py-2 px-2 rounded-r-lg hover:bg-orange-100 text-slate-500 hover:text-orange-500 transform duration-200 cursor-pointer"
+            >
+              <div className="pl-4 hover:border-r-[3px] hover:border-orange-500 flex items-center gap-x-3">
+                <BsCartFill className="scale-110" />
+                <span>{m}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default SideNavigation;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,16 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // src 미사용 시 대상 디렉토리를 명시
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // src 디렉토리 사용 시
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
-export default config;
